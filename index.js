fe4528715efa50018a197f238e443e6edfb94aec
Hardikkumar Diyora <hardikkumar.diyora@share-now.com>
2021-02-16 09:15:07 +0100

    PATCH: code

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ let app = {};
 
 // Configuration
 app.config = {
-    'timeBetweenJokes' : 1000
+    'timeBetweenJokes': 1000
 };
 
 // Function that prints a random joke
-app.printAJoke = function(){
+app.printAJoke = function () {
 
     // Get all the jokes
     let allJokes = jokesLib.allJokes();
@@ -22,7 +22,7 @@ app.printAJoke = function(){
     let numberOfJokes = allJokes.length;
 
     // Pick a random number between 1 and the number of jokes
-    let randomNumber = mathLib.getRandomNumber(1,numberOfJokes);
+    let randomNumber = mathLib.getRandomNumber(1, numberOfJokes);
 
     // Get the joke at that position in the array (minus one)
     let selectedJoke = allJokes[randomNumber - 1];
@@ -33,10 +33,10 @@ app.printAJoke = function(){
 
 
 // Function that loops indefinitely, calling the printAJoke function as it goes
-app.indefiniteLoop = function(){
+app.indefiniteLoop = function () {
 
     // Create the interval, using the config letiable defined above
-    setInterval(app.printAJoke,app.config.timeBetweenJokes);
+    setInterval(app.printAJoke, app.config.timeBetweenJokes);
 };
 
 
